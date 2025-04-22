f9ac414bb09b40580a1aab09efb32f165ce75b63
clinnyp <cphilathong1@gmail.com>
2025-04-22 18:03:02 +1200

    add login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,11 @@
+import { LoginForm } from "@/components/login-form";
+
 export default function page() {
-  return <div>login</div>;
+  return (
+    <div className="flex w-full min-h-svh justify-center items-center">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
+    </div>
+  );
 }
