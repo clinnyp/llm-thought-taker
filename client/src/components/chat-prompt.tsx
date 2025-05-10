07a8cf3e11821ab211b827084475fb594a1e79d5
clinnyp <cphilathong1@gmail.com>
2025-05-10 14:31:51 +1200

    add prompt question feature

---
+++ client/src/components/chat-prompt.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import PromptForm from "./prompt-form";
+
+export default function ChatPrompt() {
+  return (
+    <div className="flex w-3xl p-8">
+      <PromptForm />
+    </div>
+  );
+}
---
+++ client/src/components/send-button.tsx
@@ -0,0 +1,10 @@
+import { ArrowUp } from "lucide-react";
+import React from "react";
+
+export default function SendButton() {
+  return (
+    <div className="border-1 p-1 rounded-2xl border-black hover:bg-gray-100">
+      <ArrowUp />
+    </div>
+  );
+}
