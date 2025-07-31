efc58ae147da4d973cd3a8a48cd1b5856a81657a
clinnyp <cphilathong1@gmail.com>
2025-07-31 19:53:46 +1200

    rejig route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,7 @@
-"use client";
-import ChatArea from "@/components/chat-area";
-import ChatPrompt from "@/components/chat-prompt";
-import { useSelector } from "react-redux";
-import { RootState } from "@/store/store";
+import { redirect } from "next/navigation";
+import React from "react";
 
 export default function Page() {
-  const loading = useSelector((state: RootState) => state.messages.loading);
-  const chatResponse = useSelector(
-    (state: RootState) => state.messages.response
-  );
-
-  return (
-    <div className="flex h-screen w-full flex-col justify-center items-center ">
-      {loading || chatResponse ? <ChatArea /> : <ChatPrompt />}
-    </div>
-  );
+  redirect("/chat");
+  return <></>;
 }
