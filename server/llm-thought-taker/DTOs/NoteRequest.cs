b4be8f678a4ceefd349b8113f51f477b2e841372
clinnyp <cphilathong1@gmail.com>
2025-10-21 21:53:03 +1300

    fix dto to no need externalid

--- server/llm-thought-taker/DTOs/NoteRequest.cs
+++ server/llm-thought-taker/DTOs/NoteRequest.cs
@@ -2,7 +2,6 @@ namespace llm_thought_taker.DTOs;
 
 public class NoteRequest
 {
-    public string? ExternalUserId { get; set; }
     public string? Title { get; set; }
     public string? Prompt { get; set; }
     public string? Content { get; set; }
